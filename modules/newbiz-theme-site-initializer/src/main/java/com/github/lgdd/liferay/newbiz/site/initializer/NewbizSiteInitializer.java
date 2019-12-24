460ef36a0e9e242e53b293b6ef89866bc67ebabe
lgdd <louis-guillaume.durand@liferay.com>
2019-12-24 13:56:32 +0100

    Update site initializer

--- modules/newbiz-theme-site-initializer/src/main/java/com/github/lgdd/liferay/newbiz/site/initializer/NewbizSiteInitializer.java
+++ modules/newbiz-theme-site-initializer/src/main/java/com/github/lgdd/liferay/newbiz/site/initializer/NewbizSiteInitializer.java
@@ -1,212 +1,364 @@
 package com.github.lgdd.liferay.newbiz.site.initializer;
 
-import com.github.lgdd.liferay.newbiz.api.FileImporter;
-import com.github.lgdd.liferay.newbiz.api.SiteInitializerDependencyResolver;
-import com.github.lgdd.liferay.newbiz.api.SiteInitializerDependencyResolverThreadLocal;
+import com.liferay.fragment.contributor.FragmentCollectionContributorTracker;
+import com.liferay.fragment.importer.FragmentsImporter;
 import com.liferay.fragment.model.FragmentEntry;
+import com.liferay.fragment.model.FragmentEntryLink;
+import com.liferay.fragment.renderer.FragmentRenderer;
+import com.liferay.fragment.renderer.FragmentRendererTracker;
+import com.liferay.fragment.service.FragmentEntryLinkLocalService;
+import com.liferay.fragment.service.FragmentEntryLocalService;
+import com.liferay.layout.page.template.service.LayoutPageTemplateStructureLocalService;
+import com.liferay.layout.util.LayoutCopyHelper;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONArray;
-import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.LayoutConstants;
 import com.liferay.portal.kernel.model.Theme;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
 import com.liferay.portal.kernel.service.*;
-import com.liferay.portal.kernel.util.FileUtil;
-import com.liferay.portal.kernel.util.LocaleUtil;
-import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.portal.kernel.transaction.TransactionCommitCallbackUtil;
+import com.liferay.portal.kernel.util.*;
 import com.liferay.site.exception.InitializationException;
 import com.liferay.site.initializer.SiteInitializer;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 import javax.servlet.ServletContext;
 import java.io.File;
-import java.io.InputStream;
-import java.util.Locale;
-import java.util.Map;
-import java.util.ResourceBundle;
+import java.io.IOException;
+import java.net.URL;
+import java.util.*;
 
-@Component(
-	immediate = true,
-	property = "site.initializer.key=" + NewbizSiteInitializer.KEY,
-	service = SiteInitializer.class
-)
+/**
+ * Site Initializer for NewBiz theme.
+ * See https://github.com/liferay/liferay-portal/tree/7.2.x/modules/apps/site/site-buildings-site-initializer
+ */
+@Component(immediate = true,
+					 property = "site.initializer.key=" + NewbizSiteInitializer.KEY,
+					 service = SiteInitializer.class)
 public class NewbizSiteInitializer implements SiteInitializer {
+
+	public static final String KEY = "newbiz-theme";
 
 	@Override
 	public String getDescription(Locale locale) {
-		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
-										"content.Language", locale, getClass());
+
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle("content.Language", locale, getClass());
 		return LanguageUtil.get(resourceBundle, "newbiz-description");
 	}
 
 	@Override
 	public String getKey() {
+
 		return KEY;
 	}
 
 	@Override
 	public String getName(Locale locale) {
-		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
-										"content.Language", locale, getClass());
+
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle("content.Language", locale, getClass());
 		return LanguageUtil.get(resourceBundle, "newbiz");
 	}
 
 	@Override
 	public String getThumbnailSrc() {
-		_log.info("Servlet Context Path: " + _servletContext.getContextPath());
+
 		return _servletContext.getContextPath() + "/images/thumbnail.jpg";
 	}
 
 	@Override
 	public void initialize(long groupId)
 									throws InitializationException {
+
 		try {
-			SiteInitializerDependencyResolver
-											siteInitializerDependencyResolver =
-											SiteInitializerDependencyResolverThreadLocal.
-																			getSiteInitializerDependencyResolver();
+			_createServiceContext(groupId);
+			_addFragments();
 
-			if (siteInitializerDependencyResolver != null) {
-				_siteInitializerDependencyResolver =
-												siteInitializerDependencyResolver;
-			}
+			_addLayouts();
+			_updateLookAndFeel();
+			_updateLogo();
 
-			ServiceContext serviceContext = getServiceContext(groupId);
-
-			_updateLogo(serviceContext);
-			_updateLookAndFeel(serviceContext);
-			Map<String, FragmentEntry> fragmentEntriesMap = _importFragments(serviceContext);
-			_createLayouts(fragmentEntriesMap, serviceContext);
-
-		}
-		catch (InitializationException ie) {
-			throw ie;
 		}
 		catch (Exception e) {
 			_log.error(e, e);
+
 			throw new InitializationException(e);
 		}
 	}
 
-	private void _createLayouts(
-									Map<String, FragmentEntry> fragmentEntriesMap, ServiceContext serviceContext)
-									throws Exception {
-		_newbizLayoutsInitializer.initialize(fragmentEntriesMap, serviceContext);
-	}
-
 	@Override
 	public boolean isActive(long companyId) {
-		Theme theme = _themeLocalService.fetchTheme(
-										companyId, THEME_ID);
+
+		Theme theme = _themeLocalService.fetchTheme(companyId, _THEME_ID);
 
 		if (theme == null) {
 			if (_log.isInfoEnabled()) {
-				_log.info(THEME_ID + " is not registered");
+				_log.info(_THEME_ID + " is not registered");
 			}
-
 			return false;
 		}
-
 		return true;
 	}
 
-	private Map<String, FragmentEntry> _importFragments(ServiceContext serviceContext)
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+
+		_bundle = bundleContext.getBundle();
+		if (_log.isInfoEnabled()) {
+			_log.info("Bundle ID: " + _bundle.getBundleId());
+			_log.info("Bundle SymbolicName: " + _bundle.getSymbolicName());
+		}
+	}
+
+	private void _addFragments()
+									throws InitializationException {
+
+		URL url = _bundle.getEntry("/fragments.zip");
+
+		try {
+			File file = FileUtil.createTempFile(url.openStream());
+			_fragmentsImporter.importFile(_serviceContext.getUserId(), _serviceContext.getScopeGroupId(), 0, file, false);
+		}
+		catch (Exception e) {
+			throw new InitializationException(e);
+		}
+	}
+
+	private void _addLayouts()
 									throws Exception {
 
-		JSONArray jsonArray = _getJSONArray("fragments.json");
+		_addLayout(LayoutConstants.DEFAULT_PARENT_LAYOUT_ID, "Home", LayoutConstants.TYPE_CONTENT, "home.json");
 
-		Map<String, FragmentEntry> fragmentEntriesMap = _fileImporter.createFragments(jsonArray,
-										_siteInitializerDependencyResolver.getDocumentsClassLoader(),
-										_siteInitializerDependencyResolver.getFragmentsDependencyPath(), serviceContext);
-
-		return fragmentEntriesMap;
 	}
 
-	private void _updateLogo(ServiceContext serviceContext) throws Exception {
-		ClassLoader classLoader =
-										_siteInitializerDependencyResolver.getImageClassLoader();
+	private Layout _addLayout(
+									long parentLayoutId, String name, String type, String dataPath)
+									throws Exception {
 
-		InputStream inputStream = classLoader.getResourceAsStream(
-										_siteInitializerDependencyResolver.getImageDependencyPath() +
-																		"logo.jpg");
+		Map<Locale, String> nameMap = new HashMap<>();
 
-		File file = FileUtil.createTempFile(inputStream);
+		nameMap.put(LocaleUtil.getSiteDefault(), name);
 
-		_fileImporter.updateLogo(file, true, true, serviceContext);
-	}
+		Layout layout = _layoutLocalService.addLayout(_serviceContext.getUserId(), _serviceContext.getScopeGroupId(), false,
+										parentLayoutId, nameMap, new HashMap<>(), new HashMap<>(), new HashMap<>(), new HashMap<>(), type,
+										null, false, false, new HashMap<>(), _serviceContext);
 
-	private void _updateLookAndFeel(ServiceContext serviceContext)
-									throws PortalException {
+		if (Validator.isNotNull(dataPath)) {
+			Layout draftLayout = _layoutLocalService.fetchLayout(_portal.getClassNameId(Layout.class), layout.getPlid());
 
-		Theme theme = _themeLocalService.fetchTheme(
-										serviceContext.getCompanyId(), THEME_ID);
-
-		if (theme == null) {
-			if (_log.isInfoEnabled()) {
-				_log.info("No theme found for " + THEME_ID);
-			}
-
-			return;
+			_layoutPageTemplateStructureLocalService.
+											addLayoutPageTemplateStructure(_serviceContext.getUserId(), _serviceContext.getScopeGroupId(),
+																			_portal.getClassNameId(Layout.class), draftLayout.getPlid(),
+																			_parseLayoutContent(draftLayout.getPlid(), _readFile("/layouts/" + dataPath)),
+																			_serviceContext);
 		}
 
-		_layoutSetLocalService.updateLookAndFeel(
-										serviceContext.getScopeGroupId(), THEME_ID, StringPool.BLANK,
-										StringPool.BLANK);
+		TransactionCommitCallbackUtil.registerCallback(() -> {
+			_copyLayout(layout);
+
+			return null;
+		});
+
+		return layout;
 	}
 
+	private void _copyLayout(Layout layout)
+									throws Exception {
 
-	private JSONArray _getJSONArray(String name) throws Exception {
-		return _jsonFactory.createJSONArray(
-										_siteInitializerDependencyResolver.getJSON(name));
+		Layout draftLayout = _layoutLocalService.fetchLayout(_portal.getClassNameId(Layout.class), layout.getPlid());
+
+		if (draftLayout != null) {
+			_layoutCopyHelper.copyLayout(draftLayout, layout);
+		}
+
+		_layoutLocalService.updateLayout(layout.getGroupId(), layout.isPrivateLayout(), layout.getLayoutId(), new Date());
 	}
 
-	private JSONObject _getJSONObject(String name) throws Exception {
-		return _jsonFactory.createJSONObject(
-										_siteInitializerDependencyResolver.getJSON(name));
-	}
-
-	protected ServiceContext getServiceContext(long groupId)
-									throws PortalException {
-
-		User user = _userLocalService.getUser(PrincipalThreadLocal.getUserId());
-		Group group = _groupLocalService.getGroup(groupId);
-
-		Locale locale = LocaleUtil.getSiteDefault();
+	private void _createServiceContext(long groupId)
+									throws InitializationException {
 
 		ServiceContext serviceContext = new ServiceContext();
 
 		serviceContext.setAddGroupPermissions(true);
 		serviceContext.setAddGuestPermissions(true);
-		serviceContext.setCompanyId(group.getCompanyId());
+
+		User user;
+		try {
+			user = _userLocalService.getUser(PrincipalThreadLocal.getUserId());
+		}
+		catch (PortalException e) {
+			_log.error(e);
+			throw new InitializationException(e);
+		}
+
+		Locale locale = LocaleUtil.getSiteDefault();
+
 		serviceContext.setLanguageId(LanguageUtil.getLanguageId(locale));
+
 		serviceContext.setScopeGroupId(groupId);
 		serviceContext.setTimeZone(user.getTimeZone());
 		serviceContext.setUserId(user.getUserId());
 
-		return serviceContext;
+		_serviceContext = serviceContext;
 	}
 
-	@Reference
-	private JSONFactory _jsonFactory;
+	private String _parseLayoutContent(long plid, String data)
+									throws Exception {
+
+		JSONObject dataJSONObject = JSONFactoryUtil.createJSONObject(data);
+
+		JSONArray structureJSONArray = dataJSONObject.getJSONArray("structure");
+
+		for (int i = 0; i < structureJSONArray.length(); i++) {
+			JSONObject rowJSONObject = structureJSONArray.getJSONObject(i);
+
+			JSONArray columnsJSONArray = rowJSONObject.getJSONArray("columns");
+
+			for (int j = 0; j < columnsJSONArray.length(); j++) {
+				JSONObject columnJSONObject = columnsJSONArray.getJSONObject(j);
+
+				JSONArray fragmentEntriesJSONArray = columnJSONObject.getJSONArray("fragmentEntries");
+
+				JSONArray fragmentEntryLinkIdsJSONArray = JSONFactoryUtil.createJSONArray();
+
+				for (int k = 0; k < fragmentEntriesJSONArray.length(); k++) {
+					JSONObject fragmentEntryJSONObject = fragmentEntriesJSONArray.getJSONObject(k);
+
+					String fragmentEntryKey = fragmentEntryJSONObject.getString("fragmentEntryKey");
+
+					String editableValues = fragmentEntryJSONObject.getString("editableValues");
+
+					editableValues = StringUtil.replace(editableValues, StringPool.DOLLAR, StringPool.DOLLAR, _resourcesMap);
+
+					FragmentEntryLink fragmentEntryLink = _addFragmentEntryLink(plid, fragmentEntryKey, editableValues);
+
+					if (fragmentEntryLink != null) {
+						fragmentEntryLinkIdsJSONArray.put(fragmentEntryLink.getFragmentEntryLinkId());
+					}
+				}
+
+				columnJSONObject.remove("fragmentEntries");
+
+				columnJSONObject.put("fragmentEntryLinkIds", fragmentEntryLinkIdsJSONArray);
+			}
+		}
+
+		return StringUtil.replace(dataJSONObject.toString(), StringPool.DOLLAR, StringPool.DOLLAR, _resourcesMap);
+	}
+
+	private FragmentEntryLink _addFragmentEntryLink(
+									long plid, String fragmentEntryKey, String editableValues)
+									throws PortalException {
+
+		FragmentEntry fragmentEntry = _getFragmentEntry(fragmentEntryKey);
+
+		FragmentRenderer fragmentRenderer = _fragmentRendererTracker.getFragmentRenderer(fragmentEntryKey);
+
+		if ((fragmentEntry == null) && (fragmentRenderer == null)) {
+			return null;
+		}
+
+		if (fragmentEntry != null) {
+			String contributedRendererKey = null;
+
+			if (fragmentEntry.getFragmentEntryId() == 0) {
+				contributedRendererKey = fragmentEntryKey;
+			}
+
+			return _fragmentEntryLinkLocalService.addFragmentEntryLink(_serviceContext.getUserId(),
+											_serviceContext.getScopeGroupId(), 0, fragmentEntry.getFragmentEntryId(),
+											_portal.getClassNameId(Layout.class), plid, fragmentEntry.getCss(), fragmentEntry.getHtml(),
+											fragmentEntry.getJs(), fragmentEntry.getConfiguration(), editableValues, StringPool.BLANK, 0,
+											contributedRendererKey, _serviceContext);
+		}
+
+		return _fragmentEntryLinkLocalService.addFragmentEntryLink(_serviceContext.getUserId(),
+										_serviceContext.getScopeGroupId(), 0, 0, _portal.getClassNameId(Layout.class), plid,
+										StringPool.BLANK, StringPool.BLANK, StringPool.BLANK, StringPool.BLANK, editableValues,
+										StringPool.BLANK, 0, fragmentEntryKey, _serviceContext);
+	}
+
+	private FragmentEntry _getFragmentEntry(String fragmentEntryKey) {
+
+		FragmentEntry fragmentEntry =
+										_fragmentEntryLocalService.fetchFragmentEntry(_serviceContext.getScopeGroupId(), fragmentEntryKey);
+
+		if (fragmentEntry != null) {
+			return fragmentEntry;
+		}
+
+		Map<String, FragmentEntry> fragmentEntries = _fragmentCollectionContributorTracker.getFragmentEntries();
+
+		return fragmentEntries.get(fragmentEntryKey);
+	}
+
+	private void _updateLookAndFeel()
+									throws PortalException {
+
+		_layoutSetLocalService.updateLookAndFeel(_serviceContext.getScopeGroupId(), _THEME_ID, StringPool.BLANK,
+										StringPool.BLANK);
+	}
+
+	private void _updateLogo()
+									throws Exception {
+
+		Class<?> clazz = getClass();
+		byte[] logoAsBytes = FileUtil.getBytes(clazz, _DEPENDENCIES_PATH + "/images/logo.jpg");
+		_layoutSetLocalService.updateLogo(_serviceContext.getScopeGroupId(), true, true, logoAsBytes);
+	}
+
+	private String _readFile(String fileName)
+									throws IOException {
+
+		Class<?> clazz = getClass();
+
+		return StringUtil.read(clazz.getClassLoader(), _DEPENDENCIES_PATH + fileName);
+	}
+
+	private static final String _THEME_ID = "newbiz_WAR_newbiztheme";
+	private static final String _DEPENDENCIES_PATH =
+									"com/github/lgdd/liferay/newbiz/site/initializer/internal/dependencies";
+
+	private static final Log _log = LogFactoryUtil.getLog(NewbizSiteInitializer.class);
+
+	private Bundle _bundle;
+	private ServiceContext _serviceContext;
+	private final Map<String, String> _resourcesMap = new HashMap<>();
 
 	@Reference
-	private FileImporter _fileImporter;
+	FragmentEntryLocalService _fragmentEntryLocalService;
 
 	@Reference
-	private NewbizLayoutsInitializer _newbizLayoutsInitializer;
+	FragmentEntryLinkLocalService _fragmentEntryLinkLocalService;
 
 	@Reference
-	private LayoutSetLocalService _layoutSetLocalService;
+	FragmentCollectionContributorTracker _fragmentCollectionContributorTracker;
 
 	@Reference
-	private GroupLocalService _groupLocalService;
+	FragmentRendererTracker _fragmentRendererTracker;
+
+	@Reference
+	LayoutPageTemplateStructureLocalService _layoutPageTemplateStructureLocalService;
+
+	@Reference
+	LayoutCopyHelper _layoutCopyHelper;
+
+	@Reference
+	LayoutSetLocalService _layoutSetLocalService;
+
+	@Reference
+	LayoutLocalService _layoutLocalService;
 
 	@Reference
 	private UserLocalService _userLocalService;
@@ -214,19 +366,13 @@ public class NewbizSiteInitializer implements SiteInitializer {
 	@Reference
 	private ThemeLocalService _themeLocalService;
 
-	@Reference(
-		target = "(site.initializer.key=" + NewbizSiteInitializer.KEY + ")"
-	)
-	private SiteInitializerDependencyResolver _siteInitializerDependencyResolver;
+	@Reference
+	private FragmentsImporter _fragmentsImporter;
 
-	@Reference(
-		target = "(osgi.web.symbolicname=com.github.lgdd.liferay.newbiz.site.initializer)"
-	)
+	@Reference
+	private Portal _portal;
+
+	@Reference(target = "(osgi.web.symbolicname=com.github.lgdd.liferay.newbiz.site.initializer)")
 	private ServletContext _servletContext;
 
-	private static final Log _log = LogFactoryUtil.getLog(NewbizSiteInitializer.class);
-
-	private static final String THEME_ID = "newbiz_WAR_newbiztheme";
-
-	public static final String KEY = "newbiz-initializer";
 }
